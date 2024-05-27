41fcd01a96b633665186f20ba9ecaf66f309f8b3
MADUKA  C.  SALVATION <salvationmaduka1@gmail.com>
2024-05-27 05:25:17 +0100

    Update loading.jsx

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -5,8 +5,8 @@ const Loading = () => {
   return (
     <div className="loading">
        <p>Please wait...</p>
-      {/* {<iframe src="https://lottie.host/embed/8ddd90bd-5efb-456b-beab-54c530c39fef/rJa7pdcpMk.json"
-              width="400" height="400" frameborder="0" scrolling="no"></iframe>} */}
+      {<iframe src="https://lottie.host/embed/8ddd90bd-5efb-456b-beab-54c530c39fef/rJa7pdcpMk.json"
+              width="400" height="400" frameborder="0" scrolling="no"></iframe>}
     </div>
   );
 };
